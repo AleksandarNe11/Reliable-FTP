8e0aed41cb88c953cc9445f7b11dce48d25fcdf1
AleksandarNe11 <aleks.neceski@gmail.com>
2022-01-30 13:34:19 -0500

    Project Commit

---
+++ Receiver.java
@@ -0,0 +1,160 @@
+import javax.xml.crypto.Data;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class Receiver {
+    public static void main(String[] args) throws IOException {
+
+        int argCounter = 0;
+
+        // Set variables used for Receiver - base values for use in case if limited
+        // input to command line parameters
+        String sender_ip = "127.0.0.1", file_name = "write_to.txt";
+        int receiver_port = 4444, sender_port = 4443;
+        DatagramSocket dg_socket;
+        DatagramPacket dg_packet;
+        FileWriter fw;
+
+        // Parse program arguments into key variables
+        for (String output : args) {
+            switch (argCounter) {
+                case 0:
+                    sender_ip = args[argCounter];
+                    break;
+                case 1:
+                    receiver_port = Integer.parseInt(args[argCounter]);
+                    break;
+                case 2:
+                    sender_port = Integer.parseInt(args[argCounter]);
+                    break;
+                case 3:
+                    file_name = args[argCounter];
+                    break;
+            }
+            argCounter++;
+        }
+
+
+
+
+
+
+        // *TODO* wrap this behaviour in an if-else statement based on the size of the packet??
+        // *TODO* Incoming - have it constantly looping and waiting on input - no TimeOut int
+        // *TODO* needed here
+        while (true) {
+            //-----------------------------------------------------------------------------
+            // outer loop includes behaviour for the isAlive connection - this is required
+            // for the sender to connnect
+            //-----------------------------------------------------------------------------
+
+
+            // establishes DatagramSocket at the receiver_port number on this machine
+            dg_socket = new DatagramSocket(receiver_port);
+
+            // This is the writer for the file
+            fw = new FileWriter(file_name, true);
+
+            // these are the two buffers needed to create the DatagramPackets
+            // one empty for the receipt of new data bytes
+            // one non-empty for the sending bytes (not 100% sure why though)
+            byte[] buf = new byte[2];
+            byte[] send = {13, 18};
+            // Creating the packet to fill upon receipt from sender
+            dg_packet = new DatagramPacket(buf, 2);
+            // packet receipt from sender
+            dg_socket.receive(dg_packet);
+
+            // test packet received:
+            String msg = new String(buf);
+
+            // sends initial response packet to Sender side w/ buffer send
+            DatagramPacket send_dg_packet = new DatagramPacket(send,
+                    2, dg_packet.getAddress(), dg_packet.getPort());
+            dg_socket.send(send_dg_packet);
+
+            // this is the loop entered after the handshake isAlive connection
+            // this loop will call the write datagram to file after validation
+            int sequence_number = 0;
+
+            boolean measure_start = true;
+            long startTime = 0;
+            while (true) {
+                // this is the buffer and packet for file storage
+                buf = new byte[18];
+                dg_packet = new DatagramPacket(buf, buf.length);
+
+                dg_socket.receive(dg_packet);
+
+                //System.out.println(dg_packet.getLength());
+                //System.out.println("received " + new String(dg_packet.getData()));
+                // measures the start time only on the first loop
+                if (measure_start) startTime = System.currentTimeMillis();
+
+                // if the sequence number of the packet matches the expected sequence
+                // number, then write the datagram to the file and change the sequence #
+                if (dg_packet.getData()[0] == sequence_number) {
+                    write_datagram_to_file(dg_packet, fw);
+                    send_ack(dg_socket, send_dg_packet, sequence_number);
+                    // to alternate between two sequence #s we use mod 2 on an
+                    // incrementally increasing function
+                    sequence_number = (sequence_number + 1) % 2;
+                } else {
+                    if (dg_packet.getData()[0] == 2) {
+                        break;
+                    }
+                    send_ack(dg_socket, send_dg_packet, sequence_number);
+                }
+                measure_start = false;
+            }
+            long endTime = System.currentTimeMillis();
+            long transferTime = endTime - startTime;
+            System.out.println("File transfer completed in " + transferTime + "milliseconds");
+
+            fw.flush();
+            fw.close();
+
+            dg_socket.close();
+        }
+    }
+
+    private static void write_datagram_to_file(DatagramPacket dp, FileWriter fw) throws IOException {
+        // instantiate byte arrays for data from packet and for data to convert to string
+        byte[] dp_data = dp.getData();
+        
+
+        //System.out.println("in the write program");
+        //System.out.println(dp.getLength());
+
+        int packet_length = dp.getLength();
+
+        if (packet_length == 18){   
+            byte[] data = new byte[dp_data.length - 2];
+            // copies byte array indices from 1 on in dp_data to data
+            System.arraycopy(dp_data, 2, data, 0, data.length);
+            // instantiates String str from byte data in byte[] according to the UTF-8 charset
+            String str = new String(data, StandardCharsets.UTF_8);
+            // Writes character data to the file
+            fw.write(str);
+        }
+        else{
+            byte[] data = new byte[packet_length - 2];
+            System.arraycopy(dp_data, 2, data, 0, packet_length-2);
+            String str = new String(data, StandardCharsets.UTF_8);
+            fw.write(str);
+        }
+        
+
+    }
+
+    private static void send_ack(DatagramSocket dg_socket,
+                                 DatagramPacket send_dg_packet,
+                                 int sequence_number) throws IOException {
+        // sets packet with byte value of the sequence_number to return as an ACK
+        send_dg_packet.setData(new byte[] {(byte) sequence_number, 0});
+        dg_socket.send(send_dg_packet);
+    }
+}
---
+++ Sender.java
@@ -0,0 +1,286 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Timer;
+import java.math.*;
+
+import javax.swing.JTextField;
+
+public class Sender {
+
+    /**
+     * Tests to see if a connection can be established with the sender
+     *
+     * @param port_sender - this is the senders port
+     * @param port_receiver - this is the receivers port
+     * @param IP - this is the receiver IP
+     * @return
+     * @throws IOException - stuff goes wrong
+     */
+    public static DatagramSocket test_for_life(int port_sender, int port_receiver, String IP) throws IOException {
+        DatagramSocket socket;
+        try {
+            // Create new socket and associate w/ correct port later
+            socket = new DatagramSocket(port_sender);
+
+            // Get the IP address for the Sender from the txtIPReceiver input field
+            InetAddress address = InetAddress.getByName(IP);
+
+            // Get the port from the txtPORTSender
+
+
+            // buffers
+            byte[] buf = {12, 13}; //send buffer
+            byte[] buf1 = new byte[2]; // receive buffer
+            DatagramPacket send_packet = new DatagramPacket(buf, 2, address, port_receiver);
+            DatagramPacket receive_packet = new DatagramPacket(buf1, 2);
+
+            // connect() method
+            socket.connect(address, port_receiver);
+
+            // test isBound() method
+            System.out.println("IsBound : " + socket.isBound());
+            // test isConnected() method
+            System.out.println("isConnected : " + socket.isConnected());
+            // test getInetAddress() method
+            System.out.println("InetAddress : " + socket.getInetAddress());
+            // test getPort() method
+            System.out.println("Port : " + socket.getPort());
+            // test getRemoteSocketAddress() method
+            System.out.println("Remote socket address : " +
+                    socket.getRemoteSocketAddress());
+            // test getLocalSocketAddress() method
+            System.out.println("Local socket address : " +
+                    socket.getLocalSocketAddress());
+
+            // send() method
+            socket.send(send_packet);
+            System.out.println("...packet sent successfully....");
+
+            // receive() method
+            socket.receive(receive_packet);
+            System.out.println("Received packet data : " +
+                    Arrays.toString(receive_packet.getData()));
+
+            // getLocalPort() method
+            System.out.println("Local Port : " + socket.getLocalPort());
+
+            // getLocalAddress() method
+            System.out.println("Local Address : " + socket.getLocalAddress());
+
+            // setSOTimeout() method
+            socket.setSoTimeout(50);
+
+            // getSOTimeout() method
+            System.out.println("SO Timeout : " + socket.getSoTimeout());
+        } catch (IOException e) {
+
+            return null;
+        }
+
+        return socket;
+    }
+
+    public static void send_file(DatagramSocket socket,
+                                String file_name,
+                                int timeout,
+                                boolean is_reliable,
+                                int port_receiver, String IP,
+                                JTextField txtPackageCount) throws IOException {
+        // if socket is null returns -1
+        if (socket == null) {
+            //return -1;
+            txtPackageCount.setText("No Connection");
+        }
+
+
+        //System.out.println("In Sending file");
+
+        // if is_reliable, set modulus to 1, otherwise set to 10
+        int modulus = is_reliable?1:10;
+
+        // if socket is not null, pre-emptively set the timeout since we may use it
+        // need to make sure we are dealing with proper timeout, value from GUI is microseconds, needs to be milliseconds
+        // also, timeout must be > 0, a 0 timeout is interpreted as infinite timeout
+
+        if (timeout / 1000 == 0){
+            timeout = 1;
+        }
+        else{
+            timeout = timeout / 1000;
+        }
+
+        
+        socket.setSoTimeout(timeout);
+        //socket.setSoTimeout(1);
+
+        // entering this part of the code means that the socket existed
+        // ============================================================
+
+        // instantiates the reader for the file passed
+        FileReader fileReader = new FileReader(file_name);
+
+        // Get the IP address for the Sender from the txtIPReceiver input field
+        InetAddress address = InetAddress.getByName(IP);
+
+        byte[] buf = new byte[18]; //send buffer
+        byte[] buf1 = new byte[2]; // receive buffer
+        DatagramPacket send_packet = new DatagramPacket(buf, 18, address, port_receiver);
+        DatagramPacket receive_packet_curr = new DatagramPacket(buf1, 2);
+
+        // Constant loop reading from the file transferring that data to packets then sending
+        // those packets
+        int loop_condition = 1, loop_counter = 0, total_counter = 0;
+        int offset = 0, length = 16, sequence_number = 0;
+
+
+        while (loop_condition != -1) {
+
+            loop_condition = read_from_file_to_datagram(fileReader,
+                    send_packet, offset, length, sequence_number);
+
+
+            // modulus will be 10 if this function is called w/ is_reliable == false
+            // otherwise it will be 1 and the if statement will always evaluate true
+            if (is_reliable || !(loop_counter % modulus == 0)) {
+                socket.send(send_packet);
+            }
+            // calls helper function to resend the packet if timeout is reached
+            total_counter = resend_packet_on_timeout(
+                    socket, receive_packet_curr, send_packet, total_counter);
+
+            // increases the offset variable by length characters to allow the
+            // FileReader to read the next length characters
+            offset = offset + length;
+            // alternates the sequence number state
+            sequence_number = (sequence_number + 1) % 2;
+
+            total_counter++;
+            loop_counter++;
+
+            if (loop_counter % 10 == 0) {
+                txtPackageCount.setText(String.valueOf(total_counter));
+            }
+        };
+
+        // Once loop condition is set to -1, it means that the end of the file has been reached
+        // send a packet containing value 2 at it's head to indicate to the receiver that
+        // the FileWriter can be closed and it can re-enter the await-connection state
+        send_packet.setData(new byte[] {2, 0});
+        socket.send(send_packet);
+        resend_packet_on_timeout(socket, receive_packet_curr, send_packet, total_counter);
+
+
+        txtPackageCount.setText(String.valueOf(total_counter));
+
+        return;
+    }
+
+    /**
+     * This function reads the "length" characters (length*2 bytes since each character
+     * is 2 bytes) from the offset character on and populates the data of the
+     * datagram (length*2 + 1 bytes due to the sequence number) passed to the array
+     *
+     * @param fileReader - the FileReader to read the file
+     * @param dg - the DatagramPacket to fill with the byte data
+     * @param offset - the offset to read the first byte in the file from
+     * @param length - the length of the characters to read
+     * @throws IOException - stuff goes wrong
+     */
+    private static int read_from_file_to_datagram(FileReader fileReader,
+                                                  DatagramPacket dg,
+                                                  int offset,
+                                                  int length,
+                                                  int sequence_number) throws IOException {
+        char[] cbuf = new char[length];
+
+        int text_size = fileReader.read(cbuf, 0, length);                                             
+
+//        if (fileReader.read(cbuf, 0, length) != -1) {
+        if (text_size != -1){
+            //System.out.println(fileReader.read(cbuf, 0, length));
+            
+            //System.out.println(text_size);
+            if (text_size == length){
+                // instantiates array length*2 + 2 bytes long for transfer of characters
+                // (2 bytes per char) along with sequence number (1 bytes) + empty byte
+                byte[] dg_data = new byte[length + 2];
+                dg_data[0] =(byte) sequence_number;
+
+                //System.out.println(cbuf);
+                // stores the data from the character buffer read from the file and then
+                // copies the length*2 inputs in dg_data into the last length*2 positions
+                // in array dg_data
+                byte[] data = new String(cbuf).getBytes(StandardCharsets.UTF_8);
+                //System.out.println(data.length);
+                System.arraycopy(data, 0, dg_data, 2, length);
+
+                // sets the data in the datagram
+                dg.setData(dg_data);
+
+                // read the next buffer length
+                //text_size = fileReader.read(cbuf, 0, length);
+            }
+            else{
+                //System.out.println(cbuf);
+                //System.out.println("in the else");
+                byte[] dg_data = new byte[text_size + 2];
+                dg_data[0] =(byte) sequence_number;
+                byte[] data = new String(cbuf).getBytes(StandardCharsets.UTF_8);
+                System.arraycopy(data, 0, dg_data, 2, text_size);
+                dg.setData(dg_data);
+
+
+
+            }
+
+            return 1;
+        }
+
+        fileReader.close();
+        return -1;
+    }
+
+    /**
+     * This file
+     *
+     * @param socket - the socket to recieve from and send through
+     * @param receive_packet_curr - the DatagramPacket to put received info into
+     * @param send_packet - the DatagramPacket to send data using
+     * @throws IOException - stuff goes wrong
+     */
+    private static int resend_packet_on_timeout(DatagramSocket socket,
+                                                 DatagramPacket receive_packet_curr,
+                                                 DatagramPacket send_packet,
+                                                 int total_counter) throws IOException {
+
+        // initial parameters to loop and await the ACK response
+        // loop condition to make sure that we hit socket.receive() repeatedly until
+        // we receive an ACK
+        boolean timeout_condition = true;
+        while (timeout_condition) {
+            try {
+                // assigns received packet to the DatagramPacket specified
+                socket.receive(receive_packet_curr);
+
+                // line below was used for testing in the console
+                //System.out.println("ACK received w/ value: " + receive_packet_curr.getData()[0]);
+                // if this line is hit it means that an exception was not thrown
+                // i.e. that the packet was received, and we can exit the loop
+                timeout_condition = false;
+
+            } catch (SocketTimeoutException e) {
+                socket.send(send_packet);
+                total_counter++;
+            }
+        }
+        return total_counter;
+    }
+
+
+}
